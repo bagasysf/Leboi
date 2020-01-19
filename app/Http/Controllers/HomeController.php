5663ffae1bd899620cb134e762e717d762d80691
Bagas Yusuf Gunawan <bagasysf@gmail.com>
2020-01-20 03:46:33 +0700

    menambahkan role_id pada table user dan memberi nilai awal 1 yaitu barberman setiap registrasi baru dan menginitial 3 role barberman,cashier,admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $role = Role::create(['name' => 'barberman']);
         return view('home');
     }
 }
