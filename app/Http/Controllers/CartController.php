26dfb2f67d4fcddd05e034471f464075ed6ed317
Bagas Yusuf Gunawan <bagasysf@gmail.com>
2020-02-06 21:21:16 +0700

    berhasil menambahkan transaksi baru pada table transaksi dengan test menggunakan dd()

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function __invoke($id)
+    {
+        $products = Product::find($id);
+        // TODO: Insert to table transactions
+        // TODO : Assume that we dont have any data
+        $transactions = Transaction::create([
+            'total' => $products->price,
+            'user_id' => auth()->user()->id,
+            'status' => 'waiting',
+        ]);
+
+        dd($transactions);
+
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,5 @@ Route::get('products/create', 'ProductController@create')->name('product.create'
 Route::post('products/', 'ProductController@store')->name('product.store')->middleware('role:cashier|admin');
 Route::get('products/{id}/edit', 'ProductController@edit')->name('product.edit')->middleware('role:cashier|admin');
 Route::put('products/{id}', 'ProductController@update')->name('product.update')->middleware('role:cashier|admin');
+
+Route::post('add-to-cart/{id}', 'CartController')->name('cart');
